9d66dd0664596b83fbb01ae2fdd37fa4b733d4b4
Daniel Teale <djteale@gmail.com>
2018-09-04 09:42:32 +1000

    Uploading progress

--- caloriecounter/main.rb
+++ caloriecounter/main.rb
@@ -1,7 +1,13 @@
+require 'pstore'
 require_relative 'classes'
 
+# Loads previous data
+data_file = PStore.new('./datafiles/data.pstore')
+data_file.transaction do
+  $totals = data_file[:totals]
+end
 puts "Welcome to Calorie Counter"
-totals = []
+# totals = []
 while true
   hash = {}
   puts "What have you eaten?"
@@ -13,16 +19,23 @@ while true
   calorie = calorie.to_i
   food.calories = calorie
   hash['calories'] = calorie
-  totals << hash
-  puts totals
+  hash['date'] = food.date
+  $totals << hash
+  puts $totals
   puts "Do you want to add another item?"
   response = gets.chomp
   response.downcase!
   if response != "yes"
     break
   end
+  system('clear')
   # rescue
   #   puts "That is not a valid amount"
   # end
 end
 # food << item
+
+data_file = PStore.new('./datafiles/data.pstore')
+data_file.transaction do
+  data_file[:totals] = $totals
+end
